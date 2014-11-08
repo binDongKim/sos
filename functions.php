8e8772999043258d99b9441b04941e547e883ce7
SINN Huiseong <b@akaiv.com>
2014-11-09 06:49:11 +0900

    [refactor] THEME_PATH, THEME_URL

--- functions.php
+++ functions.php
@@ -1,4 +1,8 @@
 <?php
+if ( ! defined( 'THEME_PATH' ) ) define( 'THEME_PATH', get_template_directory() );
+if ( ! defined( 'THEME_URL' ) )  define( 'THEME_URL',  get_template_directory_uri() );
+include_once THEME_PATH . '/akaiv-wp-classes-setup.php';
+
 require_once 'inc/class.php';         // HTML의 클래스
 require_once 'inc/enqueue.php';       // 스타일시트, 자바스크립트, 파비콘, 타이틀
 require_once 'inc/init.php';          // 테마 설정, 관리자

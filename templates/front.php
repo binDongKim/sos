552119ee6aedf1bd79726455d432db4175d9b238
binDongKim <db@akaiv.com>
2014-11-14 16:53:56 +0900

    API 데이터 테스

--- templates/front.php
+++ templates/front.php
@@ -1,5 +1,28 @@
-<div class="container">
-  <div class="page-content">
-    <p>Welcome!</p>
-  </div>
-</div>
+<?php
+$param  = 'n14';
+$API = 'http://www.football-data.org/soccerseasons/354/fixtures';
+$url = $API . '?timeFrame=' . $param;
+$s_data = wp_remote_get($url);
+$arr_data = json_decode($s_data['body'], true);
+?>
+
+<table class="table">
+  <thead>
+    <tr>
+      <th>Date</th>
+      <th>MatchDay</th>
+      <th>Home</th>
+      <th>Away</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ( $arr_data as $match ) : ?>
+      <tr>
+        <td><?php echo $match['date']; ?></td>
+        <td><?php echo $match['matchday']; ?></td>
+        <td><?php echo $match['homeTeam']; ?></td>
+        <td><?php echo $match['awayTeam']; ?></td>
+      </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>

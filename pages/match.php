bb2985dae3a9dbe99f92a9ad8f0ab2008e5ee354
binDongKim <db@akaiv.com>
2014-11-21 16:57:53 +0900

    match-oftheday 템플릿으로 분리

--- pages/match.php
+++ pages/match.php
@@ -5,17 +5,7 @@ akaiv_before_post( false );
 akaiv_page_header( 'Match' ); ?>
 
 <div id="calendar" class="calendar clearfix"></div>
-<div id="match-oftheday" class="match-oftheday">
-  <table class="table-match-oftheday">
-    <thead>
-      <tr>
-        <td colspan="6">MATCH OF THE DAY</td>
-      </tr>
-    </thead>
-    <tbody>
-    </tbody>
-  </table>
-</div>
+<?php include_once THEME_PATH . '/templates/match-oftheday.php'; ?>
 
 <?php
 akaiv_after_post();
---
+++ templates/match-oftheday.php
@@ -0,0 +1,11 @@
+<div id="match-oftheday" class="match-oftheday">
+  <table class="table-match-oftheday">
+    <thead>
+      <tr>
+        <td colspan="6">MATCH OF THE DAY</td>
+      </tr>
+    </thead>
+    <tbody>
+    </tbody>
+  </table>
+</div>
